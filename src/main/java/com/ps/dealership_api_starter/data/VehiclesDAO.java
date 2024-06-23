69697b5504a8f4756212f4ec79aea754bf826218
Aycan <aycan.biilge@gmail.com>
2024-06-23 17:12:11 -0400

    created vehicle interface

--- src/main/java/com/ps/dealership_api_starter/data/VehiclesDAO.java
+++ src/main/java/com/ps/dealership_api_starter/data/VehiclesDAO.java
@@ -1,4 +1,29 @@
 package com.ps.dealership_api_starter.data;
 
+import com.ps.dealership_api_starter.models.Vehicle;
+import java.util.List;
+
 public interface VehiclesDAO {
+
+    List<Vehicle> getAllVehicles(
+            double minPrice,
+            double maxPrice,
+            String make,
+            String model,
+            int minYear,
+            int maxYear,
+            String color,
+            int minMiles,
+            int maxMiles,
+            String vehicleType
+    );
+
+    Vehicle getByVin(int vin);
+
+    Vehicle create(Vehicle vehicle);
+
+    void update(int vin, Vehicle vehicle);
+
+    void delete(int vin);
+
 }
